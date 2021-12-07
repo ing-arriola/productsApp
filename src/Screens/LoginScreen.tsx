b053a801c7720820d5123447b7e9bfa5d4dbbc39
ing-arriola <ingeniero.arriola@gmail.com>
2021-12-07 07:28:27 -0500

    Added background, logo and input texts

--- src/Screens/LoginScreen.tsx
+++ src/Screens/LoginScreen.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import Background from '../Components/Background'
+import InputText from '../Components/InputText'
+import WhiteLogo from '../Components/WhiteLogo'
 
 const LoginScreen = () => {
     return (
-        <View>
-            <Text>LoginScreen</Text>
-        </View>
+        <>
+            <Background/>
+            <WhiteLogo/>
+            <Text style={{color:'white',fontSize:20,fontWeight:'bold'}} >Login</Text>
+            <InputText label='Email' placeholderText='Enter your email' />
+            <InputText label='Password' placeholderText="Enter your password" />
+        </>
     )
 }
 
