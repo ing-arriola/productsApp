93f424176d22e6b3b2b306c4487f9e76d96ac2c6
ing-arriola <ingeniero.arriola@gmail.com>
2021-12-07 07:28:01 -0500

    Added main navigator

--- src/Navigation/MainNavigator.tsx
+++ src/Navigation/MainNavigator.tsx
@@ -9,10 +9,17 @@ const Stack = createStackNavigator();
 
 const MainNavigator = () => {
     return (
-        <Stack.Navigator>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-        <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
+        <Stack.Navigator
+            screenOptions={{
+                headerShown:false,
+                cardStyle:{
+                    backgroundColor:'white'
+                }
+            }}
+        >
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+            <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
       </Stack.Navigator>
     )
 }
