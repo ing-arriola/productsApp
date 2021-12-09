f8398715737c8587c144d63cc21c419a3f5547ff
ing-arriola <ingeniero.arriola@gmail.com>
2021-12-09 09:10:31 -0600

    using flex

--- src/Components/WhiteLogo.tsx
+++ src/Components/WhiteLogo.tsx
@@ -3,13 +3,13 @@ import { View, Text, Image } from 'react-native'
 
 const WhiteLogo = () => {
     return (
-        <>
+        <View style={{flex:1,justifyContent:'center'}} >
             <Image
                 source={require('../Assets/loginLogo.png')}
-                style={{width:'20%',height:'10%',alignSelf:'center'}}
+                style={{alignSelf:'center',height:100,width:120}}
                 
             />
-        </>
+        </View>
     )
 }
 
