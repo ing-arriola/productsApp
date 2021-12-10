25269e423f78b9f0afa7b0c4763b47fc44cdad8a
ing-arriola <ingeniero.arriola@gmail.com>
2021-12-10 08:41:09 -0600

    Added methods

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,23 +1,48 @@
-import React, { createContext } from "react";
-import { Usuario } from '../interfaces/authInterface';
+import React, { createContext, useReducer } from "react";
+import productsApi from "../api/ProductsApi";
+import { SinginResponse, Usuario, SignInData } from '../interfaces/authInterface';
+import { authenticationReducer, AuthState } from './authReducer';
 
 type AuthContextProps = {
     errorMessage:string
     token:string | null
     user:Usuario | null
     status : 'cheking' | 'authenticated' | 'not-authenticated'
-    signUp: () => void 
-    signIn: () => void
+    signIn: (signInInfo: SignInData) => void
+    signUp: () => void
     logOut: () => void
     removeError : () => void
+}
+
+const authInitialState : AuthState ={
+    status:'cheking',
+    token:null,
+    user:null,
+    errorMessage:''
 }
 
 export const AuthContext = createContext({} as AuthContextProps )
 
 export const AuthProvider = ({ children }:any) => {
+    const [state,dispatch] = useReducer(authenticationReducer,authInitialState)
+    const signIn = async( {correo,password}:SignInData ) => {
+        try {
+            const res = await productsApi.post<SinginResponse>('/auth/login',{correo,password})
+        } catch (error) {
+            console.log(error)
+        }
+    } 
+    const signUp = () => {}
+    const logOut = () => {}
+    const removeError = () => {}
+
     return (
         <AuthContext.Provider value={{
-            
+            ...state,
+            signIn,
+            signUp,
+            logOut,
+            removeError
         }} >
             {children}
         </AuthContext.Provider>
