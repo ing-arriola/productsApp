ca7203578c1ca58e112520a03bb5d8ee85700b3b
ing-arriola <ingeniero.arriola@gmail.com>
2021-12-10 09:33:41 -0600

    showing screens based on authentication status

--- src/Navigation/MainNavigator.tsx
+++ src/Navigation/MainNavigator.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../Screens/LoginScreen';
 import RegisterScreen from '../Screens/RegisterScreen';
 import ProtectedScreen from '../Screens/ProtectedScreen';
+import { AuthContext } from '../context/AuthContext';
 
 
 const Stack = createStackNavigator();
 
+
+
 const MainNavigator = () => {
+
+    const {status} = useContext(AuthContext)
+
+        console.log(status)
+
+
     return (
         <Stack.Navigator
             screenOptions={{
@@ -17,9 +26,16 @@ const MainNavigator = () => {
                 }
             }}
         >
-            <Stack.Screen name="LoginScreen" component={LoginScreen} />
-            <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-            <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
+            {status === 'authenticated' ? (
+                <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
+            ) :
+            (
+                <>
+                    <Stack.Screen name="LoginScreen" component={LoginScreen} />
+                    <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+                </>
+            )
+            }
       </Stack.Navigator>
     )
 }
