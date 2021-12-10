9c3ca5b4f1598a4b4970e69302ef19f90d3f3e72
ing-arriola <ingeniero.arriola@gmail.com>
2021-12-10 08:40:07 -0600

    removed references to redux

--- App.tsx
+++ App.tsx
@@ -2,9 +2,6 @@ import React from 'react'
 import {Provider} from 'react-redux'
 import { NavigationContainer } from '@react-navigation/native';
 import MainNavigator from './src/Navigation/MainNavigator';
-import { createStore } from 'redux';
-import reducers from './store/reducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import { AuthProvider } from './src/context/AuthContext';
 
 const AppState =  ({ children } : {children : JSX.Element | JSX.Element[] })  => {
@@ -17,13 +14,13 @@ const AppState =  ({ children } : {children : JSX.Element | JSX.Element[] })  =>
 }
 
 const App = () => {
-  const store=createStore(reducers,composeWithDevTools())
   return (
       <NavigationContainer>
         <AppState>
           <MainNavigator/> 
         </AppState>
       </NavigationContainer>
+  )
 }
 
 export default App
