15aa4d5dc448c34ae33bef290bf1a0d450b9c588
ing-arriola <ingeniero.arriola@gmail.com>
2021-12-11 07:13:03 -0600

    added mock data

--- src/Screens/ProtectedScreen.tsx
+++ src/Screens/ProtectedScreen.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useContext } from 'react'
+import { View, Text, StyleSheet ,Button} from 'react-native'
+import { AuthContext } from '../context/AuthContext'
+
 
 const ProtectedScreen = () => {
+
+    const {user,token,logOut} = useContext(AuthContext)
+
     return (
-        <View>
-            <Text>ProtectedScreen</Text>
+        <View style={styles.container} >
+            <Text style={styles.title} >ProtectedScreen</Text>
+            <Button
+                title='Logout'
+                color='#5856D6'
+                onPress={logOut}
+            />
+            <Text>
+                {JSON.stringify(user)}
+            </Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignContent:'center',
+        justifyContent:'center'
+    },
+    title:{
+        fontSize:20,
+        marginBottom:20
+    }
+});
 
 export default ProtectedScreen
