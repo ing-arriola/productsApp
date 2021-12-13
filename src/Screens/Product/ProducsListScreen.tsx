25c3271d7e526c3db8c1dae3e9c5bd1904240e58
ing-arriola <ingeniero.arriola@gmail.com>
2021-12-13 11:38:23 -0600

    Added screen to show list of products

---
+++ src/Screens/Product/ProducsListScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const ProducsListScreen = () => {
+    return (
+        <View>
+            <Text>ProducsListScreen</Text>
+        </View>
+    )
+}
+
+export default ProducsListScreen
