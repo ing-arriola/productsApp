efaafb02206e489fc4ee1d3881a8137abbd43a0a
ing-arriola <ingeniero.arriola@gmail.com>
2021-12-13 13:57:47 -0600

    Added info in ProducsListScreen

--- src/Screens/Product/ProducsListScreen.tsx
+++ src/Screens/Product/ProducsListScreen.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
+import React, { useContext } from 'react'
+import { View, Text, FlatList } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { ProductContext } from '../../context/ProductsContext';
 const ProducsListScreen = () => {
+
+    const {loadProducts,products} = useContext(ProductContext)
+    
+
     return (
-        <View>
-            <Text>ProducsListScreen</Text>
+        <View style={{flex:1, marginHorizontal:10}} >
+            <FlatList
+                data={products}
+                keyExtractor={(p)=>p._id}
+                renderItem={({item}) => (
+                    <TouchableOpacity
+                        activeOpacity={0.7}
+                    >
+                        <Text>{item.nombre}</Text>
+                    </TouchableOpacity>
+                )}
+                ItemSeparatorComponent={()=>
+                    <View style={{borderBottomColor:'#5856D6',borderBottomWidth:1}}/>
+                }
+            />
         </View>
     )
 }
