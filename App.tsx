07bb6a3a8f735558bf2dd046a3ec55f560ef8dd4
ing-arriola <ingeniero.arriola@gmail.com>
2021-12-13 11:37:18 -0600

    Added productProvider

--- App.tsx
+++ App.tsx
@@ -3,11 +3,14 @@ import {Provider} from 'react-redux'
 import { NavigationContainer } from '@react-navigation/native';
 import MainNavigator from './src/Navigation/MainNavigator';
 import { AuthProvider } from './src/context/AuthContext';
+import ProductsProvider from './src/context/ProductsContext';
 
 const AppState =  ({ children } : {children : JSX.Element | JSX.Element[] })  => {
   return (
     <AuthProvider>
-      {children}
+      <ProductsProvider>
+        {children}
+      </ProductsProvider>
     </AuthProvider>
   )
 
